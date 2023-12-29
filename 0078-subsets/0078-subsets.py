84a9513c9cc1db21beafd3f73414fb052d9dafac
saheeb_tareque <saheebtareque97@gmail.com>
2023-12-29 23:10:14 +0600

    Time: 81 ms (5.91%), Space: 17.4 MB (8.28%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,7 +3,7 @@ class Solution:
         result = []
         subsets = []
         def dfs(i):
-            if i>= len(nums):
+            if i == len(nums):
                 result.append(subsets.copy())
                 return
             subsets.append(nums[i])
@@ -13,5 +13,61 @@ class Solution:
             dfs(i+1)
         dfs(0)
         return result
+
+        # time complexity : o(n*2^n)
+        
+        # For the ppl like me who struggled with grasping the dfs recursive call, below is a simple example:
+
+# Let's say we have the list nums = [1, 2, 3] and we call the dfs function with i = 0.
+
+# The first call: dfs(0):
+# The condition i >= len(nums) is false, so we move forward.
+# nums[0] is appended to subset which is now [1].
+# A new call is made to dfs(1).
+
+# The second call: dfs(1):
+# The condition i >= len(nums) is false, so we move forward.
+# nums[1] is appended to subset which is now [1, 2].
+# A new call is made to dfs(2).
+
+# The third call: dfs(2):
+# The condition i >= len(nums) is false, so we move forward.
+# nums[2] is appended to subset which is now [1, 2, 3].
+# A new call is made to dfs(3).
+
+# The fourth call: dfs(3):
+# The condition i >= len(nums) is true, so the current subset [1, 2, 3] is appended to the res list.
+# The function returns.
+
+# The third call returns:
+# nums[2] is removed from subset which is now [1, 2].
+# A new call is made to dfs(3).
+
+# The fifth call: dfs(3):
+# The condition i >= len(nums) is true, so the current subset [1, 2] is appended to the res list.
+# The function returns.
+
+# The second call returns:
+# nums[1] is removed from subset which is now [1].
+# A new call is made to dfs(2).
+
+# The sixth call: dfs(2):
+# The condition i >= len(nums) is false, so we move forward.
+# nums[2] is appended to subset which is now [1, 3].
+# A new call is made to dfs(3).
+
+# The seventh call: dfs(3):
+# The condition i >= len(nums) is true, so the current subset [1, 3] is appended to the res list.
+# The function returns.
+
+# The sixth call returns:
+# nums[2] is removed from subset which is now [1].
+# A new call is made to dfs(3).
+
+# The eighth call: dfs(3):
+# The condition i >= len(nums) is true, so the current subset [1] is appended to the res list.
+
+# Hope this helps! 
+# And thanks Neetcode for sharing the original solution!
         
         
